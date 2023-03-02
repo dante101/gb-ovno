9e155de7bf0514ae4c1f543ba46db3b5b48c850d
dante101 <dante.101@inbox.ru>
2023-03-02 16:28:49 +0300

    dz-3

--- dz-3/Lesson_3_DZ_16.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-n = int(input("Введите кол-во чисел в массиве:  "))
-n_list = [randint(1, 10) for i in range(n)]
-print(n_list)
-k = int(input("Введите число для поиска:  "))
-n_list.count(k)
-print(f" Ваше число {k} встречаеться в кол-ве: {n_list.count(k)} ")
-    
