bc344e5b5468b246fca0ba5b00d98239c431c7e4
dante101 <dante.101@inbox.ru>
2023-03-07 16:10:18 +0300

    dz-4 and dz-5

--- total.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-array1 = ['ГБ', 'не', 'чему', 'не', 'учить', 'людей', 'даже', 'за', 'деньги', 'уроды!' ]
-array2 = []
-
-for i in array1:
-    if len(i) <= 3:
-        array2.append(i)
-        print(array2[:])
